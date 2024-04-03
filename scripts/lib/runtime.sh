b7cad5036b7a748bc9ab51ec1e8918ca1c311d0d
campbellman <cc13.engineering@gmail.com>
2024-04-03 08:55:03 -0400

    added a test for template variables which are in a state of high flux

--- scripts/lib/runtime.sh
+++ scripts/lib/runtime.sh
@@ -19,7 +19,7 @@ runtime_expectations=(
   "runtime_host#The host that a script is running on. It is initialized when dev-os is installed. It is set to 'remote' when the script is running on the remote server and 'local' when the script is running on the local machine."
   "runtime_repo_dir#The DevOS repo directory."
   "runtime_config_dir#The directory configuration files are stored."
-  "runtime_secrets_dir#The directory where secrets are stored."
+  "runtime_conf_dir#The directory where confs are stored."
   "runtime_ssh_key_name#The name of the SSH key used between environments."
   "runtime_log_dir#The directory where logs are stored."
   "runtime_apps_dir#The directory where apps live."
@@ -36,31 +36,31 @@ runtime_expectations=(
   "runtime_postgres_port#The port of the postgres server."
   "runtime_postgres_public_host#The public host of the postgres server."
 
-  # secrets
-  "secret_remote_ip#The remote ip address of the production server."
-  "secret_vultr_s3_host#The vultr s3 host."
-  "secret_vultr_s3_secret#The vultr s3 secret."
-  "secret_vultr_s3_access#The vultr s3 access key"
-  "secret_vultr_s3_store#The vultr s3 store label."
-  "secret_name#The name of this DevOS installation."
-  "secret_workspace_dir#The workspace directory."
-  "secret_github_username#The github username."
-  "secret_github_email#The github email"
-  "secret_gh_token#The github api token."
-  "secret_openai#The openai api key."
-  "secret_vultr_api_key#The vultr api key."
-  "secret_cloudflare_api_token#The cloudflare api token."
-  "secret_caprover_password#The caprover password."
-  "secret_postgres_password#The postgres password."
+  # confs
+  "conf_remote_ip#The remote ip address of the production server."
+  "conf_vultr_s3_host#The vultr s3 host."
+  "conf_vultr_s3_conf#The vultr s3 conf."
+  "conf_vultr_s3_access#The vultr s3 access key"
+  "conf_vultr_s3_store#The vultr s3 store label."
+  "conf_name#The name of this DevOS installation."
+  "conf_workspace_dir#The workspace directory."
+  "conf_github_username#The github username."
+  "conf_github_email#The github email"
+  "conf_gh_token#The github api token."
+  "conf_openai#The openai api key."
+  "conf_vultr_api_key#The vultr api key."
+  "conf_cloudflare_api_token#The cloudflare api token."
+  "conf_caprover_password#The caprover password."
+  "conf_postgres_password#The postgres password."
 
   # functions
-  "runtime_fn_fail_on_used_port#A function that fails if a port is already in use."
-  "runtime_fn_print_line#A function that prints a line the width of the user's terminal. Useful for displaying some forms of data."
-  "runtime_fn_arg_info#A function whose first argument is the aliased name of a script and second argument is the description of the script. Automatically supplied the information to the output when the user includes a --help flag."
-  "runtime_fn_arg_accept#The call pattern for this function looks like this: runtime_fn_arg_accept FLAGS LONGOPT DESCRIPTION DEFAULT. Example 1) runtime_fn_arg_accept 'r:' 'required-thing' 'Some thing I require'. Example 2) runtime_fn_arg_accept 'o?' 'optional-thing' 'Some optional thing'."
-  "runtime_fn_arg_parse#Returns an associative array where the keys are arg names and the values are parsed arg values. Example 1) runtime_fn_arg_accept 'r:' 'required-thing' 'Some thing I require'; runtime_fn_arg_parse \"\$@\"; echo \"\$(runtime_fn_get_arg "required-thing")\""
-  "runtime_fn_connect_db#A function that returns a connection string to a postgres database and throws if it can't establish a connection."
-  "runtime_fn_sync_db_names#A function that syncs the database names to the config/.dbs file."
+  "devos.fail_on_used_port#A function that fails if a port is already in use."
+  "devos.print_line#A function that prints a line the width of the user's terminal. Useful for displaying some forms of data."
+  "devos.arg_info#A function whose first argument is the aliased name of a script and second argument is the description of the script. Automatically supplied the information to the output when the user includes a --help flag."
+  "devos.arg_accept#The call pattern for this function looks like this: devos.arg_accept FLAGS LONGOPT DESCRIPTION DEFAULT. Example 1) devos.arg_accept 'r:' 'required-thing' 'Some thing I require'. Example 2) devos.arg_accept 'o?' 'optional-thing' 'Some optional thing'."
+  "devos.arg_parse#Returns an associative array where the keys are arg names and the values are parsed arg values. Example 1) devos.arg_accept 'r:' 'required-thing' 'Some thing I require'; devos.arg_parse \"\$@\"; echo \"\$(devos.get_arg "required-thing")\""
+  "devos.connect_db#A function that returns a connection string to a postgres database and throws if it can't establish a connection."
+  "devos.sync_db_names#A function that syncs the database names to the config/.dbs file."
 )
 
 # Set the runtime variables.
@@ -69,7 +69,7 @@ export runtime_repo_dir="$PWD"
 export runtime_host="$defaults_host"
 export runtime_log_dir="$defaults_log_dir"
 export runtime_config_dir="$runtime_repo_dir/config"
-export runtime_secrets_dir=$runtime_repo_dir/.secrets
+export runtime_conf_dir=$runtime_repo_dir/.conf
 export runtime_ssh_key_name=devos
 export runtime_caprover_root_domain=server.interbolt.org
 export runtime_caprover_name="interbolt"
@@ -82,65 +82,65 @@ export runtime_postgres_user=devos
 export runtime_postgres_port=5432
 export runtime_postgres_public_host=database.server.interbolt.org
 
-for secret_name in $(ls "$runtime_secrets_dir" | grep -v "^\."); do
-  export secret_"$secret_name"="$(cat "$runtime_secrets_dir/$secret_name")"
+for conf_name in $(ls "$runtime_conf_dir" | grep -v "^\."); do
+  export conf_"$conf_name"="$(cat "$runtime_conf_dir/$conf_name")"
 done
-export secret_workspace_dir="$secret_workspace_dir"
-export secret_remote_ip="$secret_remote_ip"
-export secret_vultr_s3_host="$secret_vultr_s3_host"
-export secret_vultr_s3_secret="$secret_vultr_s3_secret"
-export secret_vultr_s3_access="$secret_vultr_s3_access"
-export secret_vultr_s3_store="$secret_vultr_s3_store"
-export secret_name="$secret_name"
-export secret_github_username="$secret_github_username"
-export secret_github_email="$secret_github_email"
-export secret_gh_token="$secret_gh_token"
-export secret_openai="$secret_openai"
-export secret_vultr_api_key="$secret_vultr_api_key"
-export secret_cloudflare_api_token="$secret_cloudflare_api_token"
-export secret_caprover_password="$secret_caprover_password"
-export secret_postgres_password="$secret_postgres_password"
+export conf_workspace_dir="$conf_workspace_dir"
+export conf_remote_ip="$conf_remote_ip"
+export conf_vultr_s3_host="$conf_vultr_s3_host"
+export conf_vultr_s3_conf="$conf_vultr_s3_conf"
+export conf_vultr_s3_access="$conf_vultr_s3_access"
+export conf_vultr_s3_store="$conf_vultr_s3_store"
+export conf_name="$conf_name"
+export conf_github_username="$conf_github_username"
+export conf_github_email="$conf_github_email"
+export conf_gh_token="$conf_gh_token"
+export conf_openai="$conf_openai"
+export conf_vultr_api_key="$conf_vultr_api_key"
+export conf_cloudflare_api_token="$conf_cloudflare_api_token"
+export conf_caprover_password="$conf_caprover_password"
+export conf_postgres_password="$conf_postgres_password"
 
-export runtime_apps_dir=$secret_workspace_dir/apps
-export runtime_github_dir=$secret_workspace_dir/github
-export runtime_artifacts_dir=$secret_workspace_dir/.artifacts
+export runtime_apps_dir=$conf_workspace_dir/apps
+export runtime_github_dir=$conf_workspace_dir/github
+export runtime_artifacts_dir=$conf_workspace_dir/.artifacts
 
-runtime_fn_fail_on_used_port() {
+devos.fail_on_used_port() {
   local port="$1"
   if lsof -Pi :"$port" -sTCP:LISTEN -t >/dev/null; then
     log.warn "Port $port is already in use. skipping."
     exit 0
   fi
 }
-runtime_fn_print_line() {
+devos.print_line() {
   terminal_width=$(tput cols)
   line=$(printf "%${terminal_width}s" | tr " " "-")
   echo "$line"
 }
-runtime_fn_arg_info() {
+devos.arg_info() {
   cmdarg_info "header" "$1: $2"
 }
-runtime_fn_arg_accept() {
+devos.arg_accept() {
   cmdarg "$@"
 }
-runtime_fn_arg_parse() {
+devos.arg_parse() {
   cmdarg_parse "$@"
 }
-runtime_fn_get_arg() {
+devos.get_arg() {
   set +u
   echo "${cmdarg_cfg[$1]}"
   set -u
 }
-runtime_fn_connect_db() {
-  connection_uri="postgres://$runtime_postgres_user:$secret_postgres_password@$runtime_postgres_public_host:$runtime_postgres_port/$1"
+devos.connect_db() {
+  connection_uri="postgres://$runtime_postgres_user:$conf_postgres_password@$runtime_postgres_public_host:$runtime_postgres_port/$1"
   status=$(psql "$connection_uri" -c "SELECT 1" || echo "NOT_READY")
   if [ "$status" '==' "NOT_READY" ]; then
     log.throw "Failed to connect to the postgres db: $1."
   fi
   echo "$connection_uri"
 }
-runtime_fn_sync_db_names() {
-  connection_uri="postgres://$runtime_postgres_user:$secret_postgres_password@$runtime_postgres_public_host:$runtime_postgres_port/$runtime_postgres_db"
+devos.sync_db_names() {
+  connection_uri="postgres://$runtime_postgres_user:$conf_postgres_password@$runtime_postgres_public_host:$runtime_postgres_port/$runtime_postgres_db"
   db_names=$(psql "$connection_uri" -q -A -t -c "SELECT datname FROM pg_database")
   rm -f "$runtime_repo_dir"/config/.dbs
   for db_name in $db_names; do
@@ -149,14 +149,6 @@ runtime_fn_sync_db_names() {
     fi
   done
 }
-export runtime_fn_fail_on_used_port=runtime_fn_fail_on_used_port
-export runtime_fn_print_line=runtime_fn_print_line
-export runtime_fn_arg_info=runtime_fn_arg_info
-export runtime_fn_arg_accept=runtime_fn_arg_accept
-export runtime_fn_arg_parse=runtime_fn_arg_parse
-export runtime_fn_get_arg=runtime_fn_get_arg
-export runtime_fn_connect_db=runtime_fn_connect_db
-export runtime_fn_sync_db_names=runtime_fn_sync_db_names
 
 mkdir -p $runtime_apps_dir
 mkdir -p $runtime_github_dir
