69b783111a2fd3a4a0551eb58984a6dfef1a1b12
campbellman <cc13.engineering@gmail.com>
2024-04-16 16:32:51 -0400

    clone the whole thing

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ viTMP_REPO="${viTMP_DIR}/solos"
 viREPO_URL="https://github.com/InterBolt/solos.git"
 viUSR_LOCAL_BIN_EXECUTABLE="/usr/local/bin/solos"
 viREPO_BIN_EXECUTABLE_PATH="bin/proxy.sh"
-viSTATIC_MY_CONFIG_ROOT="$HOME/.solos"
+viCONFIG_ROOT="$HOME/.solos"
+viSOURCE_ROOT="${viCONFIG_ROOT}/source"
 
 do_clone() {
   if ! git clone "${viREPO_URL}" "${viTMP_REPO}" >/dev/null 2>&1; then
@@ -35,24 +36,24 @@ do_bin_link() {
   #
   # Fundamentally, we must clone the repo before we can source the static.sh file.
   #
-  mkdir -p "$viSTATIC_MY_CONFIG_ROOT"
+  mkdir -p "$viCONFIG_ROOT"
   #
   # Overwrite the bin files stored in the config folder.
   #
-  rm -rf "${viSTATIC_MY_CONFIG_ROOT:?}/bin"
-  mkdir -p "${viSTATIC_MY_CONFIG_ROOT:?}/bin"
-  cp -r "${viTMP_REPO}/bin/." "${viSTATIC_MY_CONFIG_ROOT:?}/bin" || exit 1
+  rm -rf "${viSOURCE_ROOT:?}"
+  mkdir -p "${viSOURCE_ROOT:?}"
+  cp -r "${viTMP_REPO}/." "${viSOURCE_ROOT:?}" || exit 1
   #
   # Use linking rather than copying for the simplest possible update process.
   #
-  if ! ln -sfv "${viSTATIC_MY_CONFIG_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH}" "${viUSR_LOCAL_BIN_EXECUTABLE}"; then
-    echo "Error: failed to link ${viSTATIC_MY_CONFIG_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH} to ${viUSR_LOCAL_BIN_EXECUTABLE}" >&2
+  if ! ln -sfv "${viSOURCE_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH}" "${viUSR_LOCAL_BIN_EXECUTABLE}" >/dev/null; then
+    echo "Error: failed to link ${viSOURCE_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH} to ${viUSR_LOCAL_BIN_EXECUTABLE}" >&2
     exit 1
   fi
   #
   # Ensure they're executable for extra safety.
   #
-  chmod +x "${viSTATIC_MY_CONFIG_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH}"
+  chmod +x "${viSOURCE_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH}"
   chmod +x "${viUSR_LOCAL_BIN_EXECUTABLE}"
 }
 
