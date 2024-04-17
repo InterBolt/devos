1d90ff8877f4df9c58e7d1b96d7da29d2dbac538
campbellman <cc13.engineering@gmail.com>
2024-04-17 18:23:19 -0400

    fix

--- bin/install.sh
+++ bin/install.sh
@@ -20,10 +20,8 @@ if [[ $1 = "--dev" ]]; then
 fi
 viTMP_DIR="$(mktemp -d 2>/dev/null)"
 viTMP_SOURCE_ROOT="${viTMP_DIR}/src"
-viSOURCE_ROOT="${viSOLOS_ROOT}/src"
 viSOLOS_ROOT="${HOME}/.solos"
-
-echo "${viREPO_BIN_EXECUTABLE_PATH}"
+viSOURCE_ROOT="${viSOLOS_ROOT}/src"
 
 do_clone() {
   if ! git clone "${viREPO_URL}" "${viTMP_SOURCE_ROOT}" >/dev/null 2>&1; then
@@ -40,10 +38,10 @@ do_bin_link() {
   mkdir -p "$viSOLOS_ROOT"
   rm -rf "${viSOURCE_ROOT:?}"
   mkdir -p "${viSOURCE_ROOT:?}"
-  cp -r "${viTMP_SOURCE_ROOT}/." "${viSOURCE_ROOT:?}" || exit 1
+  cp -r "${viTMP_SOURCE_ROOT:?}/." "${viSOURCE_ROOT:?}" || exit 1
 
   if ! ln -sfv "${viSOURCE_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH}" "${viUSR_LOCAL_BIN_EXECUTABLE}" >/dev/null; then
-    echo "Error: failed to link ${viSOURCE_ROOT:?}/${viREPO_BIN_EXECUTABLE_PATH} to ${viUSR_LOCAL_BIN_EXECUTABLE}" >&2
+    echo "Error: failed to link ${viSOURCE_ROOT}/${viREPO_BIN_EXECUTABLE_PATH} to ${viUSR_LOCAL_BIN_EXECUTABLE}" >&2
     exit 1
   fi
 
