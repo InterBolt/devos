fcacc4f69bdb101ef77e952d8ac09f330098ebbb
campbellman <cc13.engineering@gmail.com>
2024-04-17 19:02:11 -0400

    done

--- bin/lib/__source__.sh
+++ bin/lib/__source__.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# shellcheck source=cache.sh
-. lib/cache.sh
-# shellcheck source=env.sh
-. lib/env.sh
 # shellcheck source=ssh.sh
 . lib/ssh.sh
 # shellcheck source=status.sh
 . lib/status.sh
 # shellcheck source=utils.sh
 . lib/utils.sh
-# shellcheck source=validate.sh
-. lib/validate.sh
 # shellcheck source=vultr.sh
 . lib/vultr.sh
