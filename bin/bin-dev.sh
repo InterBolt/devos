62dc4399e6c5f8479173b51d7692b607bef29b86
campbellman <cc13.engineering@gmail.com>
2024-05-11 12:59:54 -0400

    ok

--- bin/bin-dev.sh
+++ bin/bin-dev.sh
@@ -3,7 +3,7 @@
 # shellcheck source=docker.sh
 . "${HOME}"/.solos/src/bin/docker.sh || exit 1
 # shellcheck source=shared/bin-host-post-fn.sh
-. "${HOME}"/.solos/src/shared/bin-host-post-fn.sh || exit 1
+. "${HOME}"/.solos/src/bin/shared/bin-host-post-fn.sh || exit 1
 
 __bin_dev__fn__main() {
   local post_behavior="$(__bin_host_post_fn__fn__determine_host_post_fn "$@")"
--- bin/bin.sh
+++ bin/bin.sh
@@ -2,7 +2,7 @@
 
 . "${HOME}"/.solos/src/bin/docker.sh || exit 1
 # shellcheck source=shared/bin-host-post-fn.sh
-. "${HOME}"/.solos/src/shared/bin-host-post-fn.sh || exit 1
+. "${HOME}"/.solos/src/bin/shared/bin-host-post-fn.sh || exit 1
 
 __bin__fn__main() {
   local post_behavior="$(__bin_host_post_fn__fn__determine_host_post_fn "$@")"
