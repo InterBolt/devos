4022d4baadbedafff0f90a522b189a089d174fb6
campbellman <cc13.engineering@gmail.com>
2024-05-31 17:35:44 -0400

    ok

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-exec </dev/tty &
-
 main() {
   if ! command -v bash >/dev/null 2>&1; then
   echo "Bash is required to install SolOS on this system." >&2
@@ -23,7 +21,7 @@ main() {
   tmp_dir="$(mktemp -d 2>/dev/null)"
   git clone "https://github.com/InterBolt/solos.git" "${tmp_dir}" >/dev/null 
   find "${tmp_dir}" -type f -exec chmod +x {} \;
-  bash "${tmp_dir}/host/installer.sh"
+  bash "${tmp_dir}/host/installer.sh" < /dev/tty
 }
 
 main
