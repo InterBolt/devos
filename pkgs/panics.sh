a90f7c6b49530ebc431aec8cbbeffdaeef470f9d
campbellman <cc13.engineering@gmail.com>
2024-06-07 23:06:54 +0000

    to complicated

--- pkgs/panics.sh
+++ pkgs/panics.sh
@@ -36,11 +36,12 @@ panics_add() {
     return 1
   fi
   local severity="${2:-"HIGH"}"
-  local nanoseconds="$(date +%s%N)"
   local timestamp="$(date +"%Y-%m-%dT%H:%M:%S")"
-  local panicfile="${panics__dir}/${nanoseconds}.${key}"
+  local panicfile="${panics__dir}/${key}"
   mkdir -p "${panics__dir}"
   cat <<EOF >"${panicfile}"
+MESSAGE:
+
 ${msg}
 
 TIME: ${timestamp}
@@ -64,92 +65,8 @@ panics_clear() {
   mkdir -p "${panics__dir}"
   return 0
 }
-panics_remove() {
-  local key_or_match_pattern="${1}"
-  local panic_count="$(panics.count)"
-  if [[ ${panic_count} -eq 0 ]]; then
-    return 1
-  fi
-  # TODO: hack because it works with single brackets not double, despite double being the
-  # TODO[c]: pattern I wanted to stick with for consistency.
-  if [ -f "${panics__dir}/"*".${key_or_match_pattern}" ]; then
-    rm -f "${panics__dir}/"*".${key_or_match_pattern}"
-  else
-    for panicfile in "${panics__dir}"/*; do
-      if grep -q "${key_or_match_pattern}" "${panicfile}"; then
-        rm -f "${panicfile}"
-      fi
-    done
-  fi
-  local final_panic_count="$(panics.count)"
-  if [[ ${panic_count} -eq ${final_panic_count} ]]; then
-    return 1
-  fi
-  return 0
-}
-# Gets all the latest panic files. So if there are two files for a given key, it only gets the latest.
-panics_print_latest() {
-  local key_or_match_pattern="${1}"
-  local printset=()
-  for panicfile in "${panics__dir}/"*; do
-    if [[ ! -f ${panicfile} ]]; then
-      continue
-    fi
-    if [[ -z ${key_or_match_pattern} ]]; then
-      printset+=("${panicfile}")
-      continue
-    fi
-    if [[ "$(basename "${panicfile}")" = *".${key_or_match_pattern}" ]]; then
-      printset+=("${panicfile}")
-    fi
-  done
-  if [[ ${#printset[@]} -gt 0 ]]; then
-    local i=0
-    for printfile in "${printset[@]}"; do
-      if [[ ${i} -gt 0 ]]; then
-        echo ""
-      fi
-      if ! cat "${printfile}"; then
-        return 2
-      fi
-      i=$((i + 1))
-    done
-    return 0
-  fi
-  for panicfile in "${panics__dir}"/*; do
-    if [[ ! -f ${panicfile} ]]; then
-      continue
-    fi
-    if grep -q "${key_or_match_pattern}" "${panicfile}"; then
-      printset+=("${panicfile}")
-    fi
-  done
-  i=0
-  for printfile in "${printset[@]}"; do
-    if [[ ${i} -gt 0 ]]; then
-      echo ""
-    fi
-    if ! cat "${printfile}"; then
-      return 2
-    fi
-    i=$((i + 1))
-  done
-  if [[ ${#printset[@]} -eq 0 ]]; then
-    return 1
-  fi
-  return 0
-}
 panics_print_all() {
-  local keys=()
   for panicfile in "${panics__dir}"/*; do
-    local key="$(basename "${panicfile}" | cut -d "." -f2)"
-    if ! echo "${keys[@]}" | grep -q "${key}"; then
-      keys+=("${key}")
-    fi
-  done
-  for key in "${keys[@]}"; do
-    if ! panics_print_latest "${key}"; then
-      return 1
-    fi
+    cat "${panicfile}"
   done
 }
