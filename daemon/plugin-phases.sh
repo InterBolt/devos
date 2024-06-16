6ad68ac07e00edf252fd3a6905e73bdb77895f53
campbellman <cc13.engineering@gmail.com>
2024-06-15 20:19:50 -0400

    organizing

--- daemon/plugin-phases.sh
+++ daemon/plugin-phases.sh
@@ -2,11 +2,77 @@
 
 . "${HOME}/.solos/src/shared/lib.sh" || exit 1
 . "${HOME}/.solos/src/daemon/shared.sh" || exit 1
+# ------------------------------------------------------------------------
+#
+# HELPER FUNCTIONS:
+#
+#-------------------------------------------------------------------------
+plugin_phases._merge_assets_args() {
+  local plugin_count="${1}"
+  local plugin_index="${2}"
+  local plugin_expanded_asset_args=($(echo "${3}" | xargs))
+  local asset_args=($(echo "${4}" | xargs))
+  local plugin_expanded_asset_arg_count="${#plugin_expanded_asset_args[@]}"
+  plugin_expanded_asset_arg_count=$((plugin_expanded_asset_arg_count / 3))
 
-# SUBPHASES:
-# Don't place these in shared quite yet, since the abstraction is a bit leaky
-# and the subphases are tightly coupled to the plugin-phases.
-plugin_phases.subphase_expanded_assets() {
+  local grouped_plugin_expanded_asset_args=()
+  local i=0
+  for plugin_expanded_asset_arg in "${plugin_expanded_asset_args[@]}"; do
+    if [[ $((i % 3)) -ne 0 ]]; then
+      i=$((i + 1))
+      continue
+    fi
+    local str=""
+    str="${str} ${plugin_expanded_asset_args[${i}]}"
+    str="${str} ${plugin_expanded_asset_args[$((i + 1))]}"
+    str="${str} ${plugin_expanded_asset_args[$((i + 2))]}"
+    grouped_plugin_expanded_asset_args+=("$(echo "${str}" | xargs)")
+    i=$((i + 1))
+  done
+  local grouped_plugin_expanded_asset_args_count="${#grouped_plugin_expanded_asset_args[@]}"
+  if [[ ${grouped_plugin_expanded_asset_args_count} -ne ${plugin_count} ]]; then
+    shared.log_error "Unexpected error - the number of expanded assets does not match the number of plugins (warning, you'll need coffee and bravery for this one)."
+    return 1
+  fi
+  echo "${asset_args[*]}" "${grouped_plugin_expanded_asset_args[${plugin_index}]}" | xargs
+}
+plugin_phases._validate_firejailed_assets() {
+  local asset_firejailed_rel_path="${1}"
+  local asset_host_path="${2}"
+  local chmod_permission="${3}"
+  if [[ -z "${asset_firejailed_rel_path}" ]]; then
+    shared.log_error "Unexpected error - empty asset firejailed path."
+    return 1
+  fi
+  if [[ "${asset_firejailed_rel_path}" =~ ^/ ]]; then
+    shared.log_error "Unexpected error - asset firejailed path must not start with a \"/\""
+    return 1
+  fi
+  if [[ ! "${chmod_permission}" =~ ^[0-7]{3}$ ]]; then
+    shared.log_error "Unexpected error - invalid chmod permission."
+    return 1
+  fi
+  if [[ ! -e ${asset_host_path} ]]; then
+    shared.log_error "Unexpected error - invalid asset host path."
+    return 1
+  fi
+}
+plugin_phases._merged_namespaced_fs() {
+  local namespace="${1}"
+  local partial_dir="${2}"
+  local merged_dir="${3}"
+  local partial_file="$(find "${partial_dir}" -type f | xargs)"
+  for partial_file in ${partial_files}; do
+    local partial_file_dir="$(dirname "${partial_file}")"
+    local partial_file_name="$(basename "${partial_file}")"
+    local merged_relative_path="${partial_file_dir#${partial_dir}}"
+    local merged_abs_dirpath="${merged_dir}${merged_relative_path}"
+    mkdir -p "${merged_abs_dirpath}"
+    local merged_file_path="${merged_abs_dirpath}/${namespace}-${partial_file_name}"
+    cp "${partial_file}" "${merged_file_path}"
+  done
+}
+plugin_phases._expand_assets() {
   local expanded_asset=($(echo "${1}" | xargs))
   local expanded_asset_path="${2}"
   local expanded_asset_permission="${3}"
@@ -26,42 +92,153 @@ plugin_phases.subphase_expanded_assets() {
   echo "${plugin_names[*]}" | xargs
   echo "${expanded_asset_args[*]}" | xargs
 }
-plugin_phases.subphase_firejail() {
+plugin_phases._firejail() {
   local phase_cache="${1}"
   local plugins=($(echo "${2}" | xargs))
-  local assets_args=($(echo "${3}" | xargs))
+  local asset_args=($(echo "${3}" | xargs))
   local expanded_asset_args=($(echo "${4}" | xargs))
-  local executable_args=($(echo "${5}" | xargs))
+  local executable_options=($(echo "${5}" | xargs))
   local merge_path="${6}"
   local firejail_options=($(echo "${7}" | xargs))
   local manifest_file="${8}"
   local aggregated_stdout_file="$(mktemp)"
   local aggregated_stderr_file="$(mktemp)"
-  local stashed_firejailed_home_dirs="$(mktemp)"
-  shared.firejail \
-    "${phase_cache}" \
-    "${expanded_asset_args[*]}" \
-    "${assets_args[*]}" \
-    "${plugins[*]}" \
-    "${firejail_options[*]}" \
-    "${executable_args[*]}" \
-    "${aggregated_stdout_file}" \
-    "${aggregated_stderr_file}" \
-    "${manifest_file}" \
-    >>"${stashed_firejailed_home_dirs}"
-  local return_code="$?"
-  if [[ ${return_code} -eq 151 ]]; then
-    return "${return_code}"
-  elif [[ ${return_code} -ne 0 ]]; then
+  local firejailed_pids=()
+  local firejailed_home_dirs=()
+  local plugin_stdout_files=()
+  local plugin_stderr_files=()
+  local plugin_index=0
+  local plugin_count="${#plugins[@]}"
+  for plugin_path in "${plugins[@]}"; do
+    if [[ ! -x ${plugin_path}/plugin ]]; then
+      shared.log_error "Unexpected error - ${plugin_path}/plugin is not an executable file."
+      return 1
+    fi
+    local plugins_dir="$(dirname "${plugin_path}")"
+    local plugin_name="$(shared.plugin_paths_to_names "${plugins[${plugin_index}]}")"
+    local plugin_phase_cache="${phase_cache}/${plugin_name}"
+    mkdir -p "${plugin_phase_cache}"
+    local merged_asset_args=($(
+      plugin_phases._merge_assets_args \
+        "${plugin_count}" \
+        "${plugin_index}" \
+        "${plugin_expanded_asset_args[*]}" \
+        "${asset_args[*]}"
+    ))
+    local merged_asset_arg_count="${#merged_asset_args[@]}"
+    local firejailed_home_dir="$(mktemp -d)"
+    local plugin_stdout_file="$(mktemp)"
+    local plugin_stderr_file="$(mktemp)"
+    for ((i = 0; i < ${merged_asset_arg_count}; i++)); do
+      if [[ $((i % 3)) -ne 0 ]]; then
+        continue
+      fi
+      local asset_firejailed_rel_path="${merged_asset_args[${i}]}"
+      local asset_host_path="${merged_asset_args[$((i + 1))]}"
+      local chmod_permission="${merged_asset_args[$((i + 2))]}"
+      if ! plugin_phases._validate_firejailed_assets \
+        "${asset_firejailed_rel_path}" \
+        "${asset_host_path}" \
+        "${chmod_permission}"; then
+        return 1
+      fi
+      cp -r "${plugin_phase_cache}" "${firejailed_home_dir}/cache"
+      local asset_firejailed_path="${firejailed_home_dir}/${asset_firejailed_rel_path}"
+      if [[ -f ${asset_host_path} ]]; then
+        cp "${asset_host_path}" "${asset_firejailed_path}"
+      elif [[ -d ${asset_host_path} ]]; then
+        mkdir -p "${asset_firejailed_path}"
+        cp -r "${asset_host_path}" "${asset_firejailed_path}/"
+      fi
+      cp -a "${plugin_path}/plugin" "${firejailed_home_dir}/plugin"
+      local plugin_config_file="${plugin_path}/solos.config.json"
+      if [[ -f ${plugin_config_file} ]]; then
+        cp "${plugin_config_file}" "${firejailed_home_dir}/solos.config.json"
+      else
+        echo "{}" >"${firejailed_home_dir}/solos.config.json"
+      fi
+      if [[ -f ${manifest_file} ]]; then
+        # TODO: make sure local plugins are included.
+        cp "${manifest_file}" "${firejailed_home_dir}/solos.manifest.json"
+      else
+        return 1
+      fi
+      if [[ ! " ${executable_options[@]} " =~ " --phase-configure " ]]; then
+        chmod 555 "${firejailed_home_dir}/solos.config.json"
+      else
+        chmod 777 "${firejailed_home_dir}/solos.config.json"
+      fi
+      chmod -R "${chmod_permission}" "${asset_firejailed_path}"
+      firejail \
+        --quiet \
+        --noprofile \
+        --private="${firejailed_home_dir}" \
+        "${firejail_options[@]}" \
+        /root/plugin "${executable_options[@]}" \
+        >>"${plugin_stdout_file}" 2>>"${plugin_stderr_file}" &
+      local firejailed_pid=$!
+      firejailed_pids+=("${firejailed_pid}")
+      firejailed_home_dirs+=("${firejailed_home_dir}")
+      plugin_stdout_files+=("${plugin_stdout_file}")
+      plugin_stderr_files+=("${plugin_stderr_file}")
+    done
+    plugin_index=$((plugin_index + 1))
+  done
+  local firejailed_kills=""
+  local firejailed_failures=0
+  local i=0
+  for firejailed_pid in "${firejailed_pids[@]}"; do
+    wait "${firejailed_pid}"
+    local firejailed_exit_code=$?
+    local executable_path="${plugins[${i}]}/plugin"
+    local plugin_name="$(shared.plugin_paths_to_names "${plugins[${i}]}")"
+    local firejailed_home_dir="${firejailed_home_dirs[${i}]}"
+    # Blanket remove the restrictions placed on the firejailed files so that
+    # our daemon can do what it needs to do with the files.
+    chmod -R 777 "${firejailed_home_dir}"
+    local plugin_stdout_file="${plugin_stdout_files[${i}]}"
+    local plugin_stderr_file="${plugin_stderr_files[${i}]}"
+    if [[ -f ${plugin_stdout_file} ]]; then
+      while IFS= read -r line; do
+        echo "(${plugin_name}) ${line}" >>"${aggregated_stderr_file}"
+      done <"${plugin_stdout_file}"
+    fi
+    if [[ -f ${plugin_stderr_file} ]]; then
+      while IFS= read -r line; do
+        echo "(${plugin_name}) ${line}" >>"${aggregated_stdout_file}"
+      done <"${plugin_stderr_file}"
+    fi
+    if [[ ${firejailed_exit_code} -ne 0 ]]; then
+      echo "Firejailed plugin error - ${executable_path} exited with status ${firejailed_exit_code}" >&2
+      firejailed_failures=$((firejailed_failures + 1))
+    fi
+    i=$((i + 1))
+  done
+  i=0
+  for plugin_stdout_file in "${plugin_stdout_files[@]}"; do
+    local plugin_name="$(shared.plugin_paths_to_names "${plugins[${i}]}")"
+    if grep -q "^SOLOS_PANIC" "${plugin_stderr_file}" >/dev/null 2>/dev/null; then
+      firejailed_kills="${firejailed_kills} ${plugin_name}"
+    fi
+    i=$((i + 1))
+  done
+  firejailed_kills=($(echo "${firejailed_kills}" | xargs))
+  for plugin_stderr_file in "${plugin_stderr_files[@]}"; do
+    if grep -q "^SOLOS_PANIC" "${plugin_stderr_file}" >/dev/null 2>/dev/null; then
+      shared.log_warn "Invalid usage - the plugin sent a panic message to stdout." >&2
+    fi
+  done
+  if [[ ${firejailed_failures} -gt 0 ]]; then
+    shared.log_error "Unexpected plugin error - there were ${firejailed_failures} total failures across ${plugin_count} plugins."
     echo "${aggregated_stdout_file}"
     echo "${aggregated_stderr_file}"
     echo "${assets_created_by_plugins[*]}"
+    return 1
+  fi
+  if [[ ${#firejailed_kills[@]} -gt 0 ]]; then
+    shared.log_warn "Unexpected plugin error - ${#firejailed_kills[@]} firejailed panic requests. Sources: ${firejailed_kills[*]}."
     return "${return_code}"
   fi
-  local firejailed_home_dirs=()
-  while read -r line; do
-    firejailed_home_dirs+=("$(echo "${line}" | xargs)")
-  done <"${stashed_firejailed_home_dirs}"
   local assets_created_by_plugins=()
   local i=0
   if [[ -n ${merge_path} ]]; then
@@ -77,8 +254,11 @@ plugin_phases.subphase_firejail() {
   echo "${aggregated_stderr_file}"
   echo "${assets_created_by_plugins[*]}"
 }
-# PHASE DEFINITIONS:
-# These should be written in the order they run.
+# ------------------------------------------------------------------------
+#
+# ALL PHASES:
+#
+#-------------------------------------------------------------------------
 #
 # The configure phase is responsible for making any modifications to the config files associated
 # with the plugins. This allows for a simple upgrade path for plugins that need to make changes
@@ -86,7 +266,7 @@ plugin_phases.subphase_firejail() {
 plugin_phases.configure() {
   local phase_cache="${1}"
   local returned="$(
-    plugin_phases.subphase_expanded_assets \
+    plugin_phases._expand_assets \
       "" \
       "" \
       "" \
@@ -99,16 +279,16 @@ plugin_phases.configure() {
   local plugins=($(lib.line_to_args "${returned}" "0"))
   local plugin_names=($(lib.line_to_args "${returned}" "1"))
   local expanded_asset_args=($(lib.line_to_args "${returned}" "2"))
-  local executable_args=("--phase-configure")
+  local executable_options=("--phase-configure")
   local firejail_args=()
   local asset_args=()
   local returned="$(
-    plugin_phases.subphase_firejail \
+    plugin_phases._firejail \
       "${phase_cache}" \
       "${plugins[*]}" \
       "${asset_args[*]}" \
       "${expanded_asset_args[*]}" \
-      "${executable_args[*]}" \
+      "${executable_options[*]}" \
       "/solos.config.json" \
       "${firejail_args[*]}" \
       "${manifest_file}" || echo "$?"
@@ -136,7 +316,7 @@ plugin_phases.configure() {
 plugin_phases.download() {
   local phase_cache="${1}"
   local returned="$(
-    plugin_phases.subphase_expanded_assets \
+    plugin_phases._expand_assets \
       "" \
       "" \
       "" \
@@ -149,18 +329,18 @@ plugin_phases.download() {
   local plugins=($(lib.line_to_args "${returned}" "0"))
   local plugin_names=($(lib.line_to_args "${returned}" "1"))
   local expanded_asset_args=($(lib.line_to_args "${returned}" "2"))
-  local executable_args=("--phase-download")
+  local executable_options=("--phase-download")
   local firejail_args=()
   local asset_args=(
     "$(mktemp -d)" "/download" "777"
   )
   local returned="$(
-    plugin_phases.subphase_firejail \
+    plugin_phases._firejail \
       "${phase_cache}" \
       "${plugins[*]}" \
       "${asset_args[*]}" \
       "${expanded_asset_args[*]}" \
-      "${executable_args[*]}" \
+      "${executable_options[*]}" \
       "/download" \
       "${firejail_args[*]}" \
       "${manifest_file}" || echo "$?"
@@ -175,7 +355,7 @@ plugin_phases.download() {
   local i=0
   for created_download_dir in "${download_dirs_created_by_plugins[@]}"; do
     local plugin_name="${plugin_names[${i}]}"
-    shared.merged_namespaced_fs \
+    plugin_phases._merged_namespaced_fs \
       "${plugin_name}" \
       "${created_download_dir}" \
       "${merge_dir}"
@@ -195,7 +375,7 @@ plugin_phases.process() {
   local merged_download_dir="${3}"
   local plugin_download_dirs=($(echo "${4}" | xargs))
   local returned="$(
-    plugin_phases.subphase_expanded_assets \
+    plugin_phases._expand_assets \
       "${plugin_download_dirs[*]}" \
       "/download" \
       "555" \
@@ -208,20 +388,20 @@ plugin_phases.process() {
   local plugins=($(lib.line_to_args "${returned}" "0"))
   local plugin_names=($(lib.line_to_args "${returned}" "1"))
   local expanded_asset_args=($(lib.line_to_args "${returned}" "2"))
-  local executable_args=("--phase-process")
+  local executable_options=("--phase-process")
   local firejail_args=("--net=none")
   local asset_args=(
     "$(mktemp)" "/processed.json" "777"
-    "${scrubbed_dir}" "/.solos" "555"
+    "${scrubbed_dir}" "/solos" "555"
     "${merged_download_dir}" "/plugins/download" "555"
   )
   local returned="$(
-    plugin_phases.subphase_firejail \
+    plugin_phases._firejail \
       "${phase_cache}" \
       "${plugins[*]}" \
       "${asset_args[*]}" \
       "${expanded_asset_args[*]}" \
-      "${executable_args[*]}" \
+      "${executable_options[*]}" \
       "/processed.json" \
       "${firejail_args[*]}" \
       "${manifest_file}" || echo "$?"
@@ -251,7 +431,7 @@ plugin_phases.chunk() {
   local merged_processed_dir="${2}"
   local processed_files=("$(echo "${3}" | xargs)")
   local returned="$(
-    plugin_phases.subphase_expanded_assets \
+    plugin_phases._expand_assets \
       "${processed_files[*]}" \
       "/processed.json" \
       "555" \
@@ -264,19 +444,19 @@ plugin_phases.chunk() {
   local plugins=($(lib.line_to_args "${returned}" "0"))
   local plugin_names=($(lib.line_to_args "${returned}" "1"))
   local expanded_asset_args=($(lib.line_to_args "${returned}" "2"))
-  local executable_args=("--phase-chunk")
+  local executable_options=("--phase-chunk")
   local firejail_args=()
   local asset_args=(
     "$(mktemp)" "/chunks.log" "777"
     "${merged_processed_dir}" "/plugins/processed" "555"
   )
   local returned="$(
-    plugin_phases.subphase_firejail \
+    plugin_phases._firejail \
       "${phase_cache}" \
       "${plugins[*]}" \
       "${asset_args[*]}" \
       "${expanded_asset_args[*]}" \
-      "${executable_args[*]}" \
+      "${executable_options[*]}" \
       "/chunks.log" \
       "${firejail_args[*]}" \
       "${manifest_file}" || echo "$?"
@@ -310,7 +490,7 @@ plugin_phases.publish() {
   local merged_chunks="${2}"
   local chunk_log_files=("$(echo "${3}" | xargs)")
   local returned="$(
-    plugin_phases.subphase_expanded_assets \
+    plugin_phases._expand_assets \
       "${chunk_log_files[*]}" \
       "/chunks.log" \
       "555" \
@@ -323,18 +503,18 @@ plugin_phases.publish() {
   local plugins=($(lib.line_to_args "${returned}" "0"))
   local plugin_names=($(lib.line_to_args "${returned}" "1"))
   local expanded_asset_args=($(lib.line_to_args "${returned}" "2"))
-  local executable_args=("--phase-publish")
+  local executable_options=("--phase-publish")
   local firejail_args=()
   local asset_args=(
     "${merged_chunks}" "/plugins/chunks" "555"
   )
   local returned="$(
-    plugin_phases.subphase_firejail \
+    plugin_phases._firejail \
       "${phase_cache}" \
       "${plugins[*]}" \
       "${asset_args[*]}" \
       "${expanded_asset_args[*]}" \
-      "${executable_args[*]}" \
+      "${executable_options[*]}" \
       "" \
       "${firejail_args[*]}" \
       "${manifest_file}" || echo "$?"
--- host/bin.sh
+++ host/bin.sh
@@ -3,52 +3,52 @@
 . "${HOME}/.solos/src/shared/lib.sh" || exit 1
 . "${HOME}"/.solos/src/host/cli-posthooks.sh || exit 1
 
-bridge__data_dir="$(lib.data_dir_path)"
-bridge__repo_dir="${HOME}/.solos/src"
-bridge__store_dir="${HOME}/.solos/data/store"
-bridge__stored_users_home_dir="${bridge__store_dir}/users_home_dir"
-bridge__last_docker_build_hash="$(lib.last_docker_build_hash_path)"
-bridge__mount_dir="/root/.solos"
-bridge__mounted_daemon_path="/root/.solos/src/daemon/bin.sh"
-bridge__mounted_cli_path="/root/.solos/src/cli/bin.sh"
-bridge__users_bashrc_path="${HOME}/.solos/rcfiles/.bashrc"
-bridge__installer_no_tty_flag=false
-bridge__shell_minimal_flag=false
-bridge__shell_full_flag=false
-bridge__cli_flag=true
-bridge__next_args=()
+bin__data_dir="$(lib.data_dir_path)"
+bin__repo_dir="${HOME}/.solos/src"
+bin__store_dir="${HOME}/.solos/data/store"
+bin__stored_users_home_dir="${bin__store_dir}/users_home_dir"
+bin__last_docker_build_hash="$(lib.last_docker_build_hash_path)"
+bin__mount_dir="/root/.solos"
+bin__mounted_daemon_path="/root/.solos/src/daemon/bin.sh"
+bin__mounted_cli_path="/root/.solos/src/cli/bin.sh"
+bin__users_bashrc_path="${HOME}/.solos/rcfiles/.bashrc"
+bin__installer_no_tty_flag=false
+bin__shell_minimal_flag=false
+bin__shell_full_flag=false
+bin__cli_flag=true
+bin__next_args=()
 
 # Map the flags to the relevant values and remove them from the args.
 for entry_arg in "$@"; do
   if [[ ${entry_arg} = "--installer-no-tty" ]]; then
-    bridge__installer_no_tty_flag=true
+    bin__installer_no_tty_flag=true
   elif [[ ${entry_arg} = "shell-minimal" ]]; then
-    bridge__shell_minimal_flag=true
-    bridge__shell_full_flag=false
-    bridge__cli_flag=false
+    bin__shell_minimal_flag=true
+    bin__shell_full_flag=false
+    bin__cli_flag=false
   elif [[ ${entry_arg} = "shell" ]]; then
-    bridge__shell_full_flag=true
-    bridge__shell_minimal_flag=false
-    bridge__cli_flag=false
+    bin__shell_full_flag=true
+    bin__shell_minimal_flag=false
+    bin__cli_flag=false
   elif [[ ${entry_arg} = "--cli" ]]; then
-    bridge__cli_flag=true
+    bin__cli_flag=true
   else
-    bridge__next_args+=("${entry_arg}")
+    bin__next_args+=("${entry_arg}")
   fi
 done
-set -- "${bridge__next_args[@]}" || exit 1
+set -- "${bin__next_args[@]}" || exit 1
 
 export DOCKER_CLI_HINTS=false
 
-bridge.error_press_enter() {
+bin.error_press_enter() {
   echo "Press enter to exit..."
   read -r || exit 1
   exit 1
 }
-bridge.hash() {
+bin.hash() {
   git -C "${HOME}/.solos/src" rev-parse --short HEAD | cut -c1-7 || echo ""
 }
-bridge.destroy() {
+bin.destroy() {
   for image_name in $(docker ps -a --format '{{.Image}}'); do
     if [[ ${image_name} = "solos:"* ]]; then
       docker stop "$(docker ps -a --format '{{.ID}}' --filter ancestor="${image_name}")" >/dev/null 2>&1
@@ -57,39 +57,39 @@ bridge.destroy() {
     fi
   done
 }
-bridge.test() {
+bin.test() {
   local container_ctx="${PWD/#$HOME//root}"
   local args=()
-  if [[ ${bridge__installer_no_tty_flag} = true ]]; then
-    args=(-i -w "${container_ctx}" "$(bridge.hash)" echo "")
+  if [[ ${bin__installer_no_tty_flag} = true ]]; then
+    args=(-i -w "${container_ctx}" "$(bin.hash)" echo "")
   else
-    args=(-it -w "${container_ctx}" "$(bridge.hash)" echo "")
+    args=(-it -w "${container_ctx}" "$(bin.hash)" echo "")
   fi
   if ! docker exec "${args[@]}" >/dev/null 2>&1; then
     return 1
   fi
   return 0
 }
-bridge.launch_daemon() {
+bin.launch_daemon() {
   local container_ctx="${PWD/#$HOME//root}"
-  local args=(-i -w "${container_ctx}" "$(bridge.hash)")
-  local bash_args=(-c 'nohup '"${bridge__mounted_daemon_path}"' >/dev/null 2>&1 &')
+  local args=(-i -w "${container_ctx}" "$(bin.hash)")
+  local bash_args=(-c 'nohup '"${bin__mounted_daemon_path}"' >/dev/null 2>&1 &')
   docker exec "${args[@]}" /bin/bash "${bash_args[@]}"
 }
-bridge.exec_shell() {
+bin.exec_shell() {
   local bashrc_file="${1:-""}"
   local container_ctx="${PWD/#$HOME//root}"
   local args=()
-  if [[ ${bridge__installer_no_tty_flag} = true ]]; then
-    args=(-i -w "${container_ctx}" "$(bridge.hash)")
+  if [[ ${bin__installer_no_tty_flag} = true ]]; then
+    args=(-i -w "${container_ctx}" "$(bin.hash)")
   else
-    args=(-it -w "${container_ctx}" "$(bridge.hash)")
+    args=(-it -w "${container_ctx}" "$(bin.hash)")
   fi
   local bash_args=()
   if [[ -n ${bashrc_file} ]]; then
     if [[ ! -f ${bashrc_file} ]]; then
       echo "The supplied bashrc file at ${bashrc_file} does not exist." >&2
-      bridge.error_press_enter
+      bin.error_press_enter
     fi
     local relative_bashrc_file="${bashrc_file/#$HOME/~}"
     bash_args=(--rcfile "${relative_bashrc_file}")
@@ -97,40 +97,40 @@ bridge.exec_shell() {
 
   docker exec "${args[@]}" /bin/bash "${bash_args[@]}" -i
 }
-bridge.exec_command() {
+bin.exec_command() {
   local container_ctx="${PWD/#$HOME//root}"
   local args=()
   local bash_args=()
-  if [[ ${bridge__installer_no_tty_flag} = true ]]; then
-    args=(-i -w "${container_ctx}" "$(bridge.hash)")
+  if [[ ${bin__installer_no_tty_flag} = true ]]; then
+    args=(-i -w "${container_ctx}" "$(bin.hash)")
     bash_args=(-c ''"${*}"'')
   else
-    args=(-it -w "${container_ctx}" "$(bridge.hash)")
+    args=(-it -w "${container_ctx}" "$(bin.hash)")
     bash_args=(-i -c ''"${*}"'')
   fi
   docker exec "${args[@]}" /bin/bash "${bash_args[@]}"
 }
-bridge.build_and_run() {
+bin.build_and_run() {
   if [[ -f ${HOME}/.solos ]]; then
     echo "A file called .solos was detected in your home directory." >&2
-    bridge.error_press_enter
+    bin.error_press_enter
   fi
-  mkdir -p "$(dirname "${bridge__stored_users_home_dir}")"
-  echo "${HOME}" >"${bridge__stored_users_home_dir}"
-  local prev_hash="$(cat "${bridge__last_docker_build_hash}" 2>/dev/null || echo "")"
-  local hash="$(bridge.hash)"
+  mkdir -p "$(dirname "${bin__stored_users_home_dir}")"
+  echo "${HOME}" >"${bin__stored_users_home_dir}"
+  local prev_hash="$(cat "${bin__last_docker_build_hash}" 2>/dev/null || echo "")"
+  local hash="$(bin.hash)"
   local build_args=(-q)
   if [[ ${prev_hash} != "${hash}" ]]; then
     build_args=()
   fi
-  if ! docker build "${build_args[@]}" -t "solos:${hash}" -f "${bridge__repo_dir}/Dockerfile" .; then
+  if ! docker build "${build_args[@]}" -t "solos:${hash}" -f "${bin__repo_dir}/Dockerfile" .; then
     echo "Unexpected error: failed to build the docker image." >&2
-    bridge.error_press_enter
+    bin.error_press_enter
   fi
-  echo "${hash}" >"${bridge__last_docker_build_hash}"
+  echo "${hash}" >"${bin__last_docker_build_hash}"
   local shared_docker_run_args=(
     --name
-    "$(bridge.hash)"
+    "$(bin.hash)"
     -d
     --network
     host
@@ -140,49 +140,49 @@ bridge.build_and_run() {
     -v
     /var/run/docker.sock:/var/run/docker.sock
     -v
-    "${HOME}/.solos:${bridge__mount_dir}"
-    "solos:$(bridge.hash)"
+    "${HOME}/.solos:${bin__mount_dir}"
+    "solos:$(bin.hash)"
   )
-  if [[ ${bridge__installer_no_tty_flag} = true ]]; then
+  if [[ ${bin__installer_no_tty_flag} = true ]]; then
     docker run -i "${shared_docker_run_args[@]}" >/dev/null &
   else
     docker run -it "${shared_docker_run_args[@]}" >/dev/null &
   fi
-  while ! bridge.test; do
+  while ! bin.test; do
     sleep .2
   done
-  bridge.launch_daemon
+  bin.launch_daemon
 }
-bridge.rebuild() {
+bin.rebuild() {
   echo -e "\033[0;34mRebuilding the container...\033[0m"
-  if ! bridge.destroy; then
+  if ! bin.destroy; then
     echo "Unexpected error: failed to cleanup old containers." >&2
-    bridge.error_press_enter
+    bin.error_press_enter
   fi
-  if ! bridge.build_and_run; then
+  if ! bin.build_and_run; then
     echo "Unexpected error: failed to build and run the container." >&2
-    bridge.error_press_enter
+    bin.error_press_enter
   fi
 }
-bridge.shell() {
-  if bridge.test; then
-    bridge.exec_shell "$@"
+bin.shell() {
+  if bin.test; then
+    bin.exec_shell "$@"
     return 0
   fi
-  bridge.rebuild
-  bridge.exec_shell "$@"
+  bin.rebuild
+  bin.exec_shell "$@"
 }
-bridge.cmd() {
-  if bridge.test; then
-    bridge.exec_command "$@"
+bin.cmd() {
+  if bin.test; then
+    bin.exec_command "$@"
     return 0
   fi
-  bridge.rebuild
-  bridge.exec_command "$@"
+  bin.rebuild
+  bin.exec_command "$@"
 }
-bridge.exec_cli() {
+bin.exec_cli() {
   local post_behavior="$(cli_posthooks.determine_command "$@")"
-  if bridge.cmd "${bridge__mounted_cli_path}" "$@"; then
+  if bin.cmd "${bin__mounted_cli_path}" "$@"; then
     if [[ -n ${post_behavior} ]]; then
       if [[ "$*" = *" --help"* ]] || [[ "$*" = *" help"* ]]; then
         return 0
@@ -193,7 +193,7 @@ bridge.exec_cli() {
     fi
   fi
 }
-bridge.cli() {
+bin.cli() {
   local curr_project="$(lib.checked_out_project)"
   local solos_cmd=""
   local args=("$@")
@@ -203,31 +203,31 @@ bridge.cli() {
     fi
   done
   if [[ -z ${solos_cmd} ]] || [[ ${solos_cmd} = "checkout" ]]; then
-    bridge.destroy
+    bin.destroy
   fi
-  bridge.exec_cli "${args[@]}"
+  bin.exec_cli "${args[@]}"
 }
-bridge.main() {
-  mkdir -p "${bridge__data_dir}"
-  if [[ ${bridge__cli_flag} = true ]]; then
+bin.main() {
+  mkdir -p "${bin__data_dir}"
+  if [[ ${bin__cli_flag} = true ]]; then
     local cli_args=()
     for entry_arg in "$@"; do
       if [[ ${entry_arg} != shell ]] && [[ ${entry_arg} != shell-* ]]; then
         cli_args+=("${entry_arg}")
       fi
     done
-    bridge.cli "${cli_args[@]}"
+    bin.cli "${cli_args[@]}"
     exit $?
-  elif [[ ${bridge__shell_minimal_flag} = true ]]; then
-    bridge.shell
+  elif [[ ${bin__shell_minimal_flag} = true ]]; then
+    bin.shell
     exit $?
-  elif [[ ${bridge__shell_full_flag} = true ]]; then
-    bridge.shell "${bridge__users_bashrc_path}"
+  elif [[ ${bin__shell_full_flag} = true ]]; then
+    bin.shell "${bin__users_bashrc_path}"
     exit $?
   else
     echo "Unexpected error: invalid, incorrect, or missing flags." >&2
-    bridge.error_press_enter
+    bin.error_press_enter
   fi
 }
 
-bridge.main "$@"
+bin.main "$@"
