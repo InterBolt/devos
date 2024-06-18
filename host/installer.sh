156b1e9cac28118350ea1e099353702cbb497031
campbellman <cc13.engineering@gmail.com>
2024-06-18 13:17:18 -0400

    next

--- host/installer.sh
+++ host/installer.sh
@@ -90,7 +90,7 @@ installer.install() {
   fi
   echo "WE HERE WHY WE ENDING" >&2
   if ! solos --installer-no-tty --restricted-noop; then
-    echo "WE HERE WHY WE ENDING" >&2
+    echo "WE HERE WHY WE ENDING-inner" >&2
     echo "SolOS installation failed." >&2
     echo "Failed to run SolOS cli after installing it." >&2
     return 1
